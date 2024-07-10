a36d23d918768fb8b2a4ff1707d9833a50fce6ab
Alex Kuropiatnyk <allkurop@gmail.com>
2024-07-10 10:37:33 +0200

    f

---
+++ main/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+#include "esp_log.h"
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#define TAG "metalbox_switch"
+
+namespace
+{
+}
+extern "C" void app_main(void)
+{
+    ESP_LOGI("TAG", ">>>>>>> connected");
+}
