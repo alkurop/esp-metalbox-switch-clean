e13fa5306d5620e93f2bacd38395774d259ebcbe
Alexey Kuropiantyk <alkurop@gmail.com>
2024-07-15 10:49:49 +0200

    create vector clas

---
+++ main/utils/vector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class Vector
+{
+public:
+    Vector(int s) : elem{new double[s]}, sz{s} {} // construct a Vector
+    double &operator[](int i) { return elem[i]; } // element access: subscripting
+    int size() { return sz; }
+
+private:
+    double *elem; // pointer to the elements
+    int sz;       // the number of elements
+};
